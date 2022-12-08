2abdfee1bd67de8a9d4808fda30db8dcc242c871
Charitha Alwis <sathruvani@gmail.com>
2022-12-08 16:49:57 -0600

    added schema files, utils auth.js and models user.js files

--- server/models/User.js
+++ server/models/User.js
@@ -5,51 +5,57 @@ const bcrypt = require('bcrypt');
 const movieSchema = require('./Movies');
 
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    
-    savedMovies: [movieSchema],
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+     
+        savedMovies: [movieSchema],
+
+    rentalStart: {
+        type: Date,
+        
+    }
+   
   },
- 
-  {
+
+{
     toJSON: {
-      virtuals: true,
+        virtuals: true,
     },
-  }
+}
 );
 
 // hash user password
 userSchema.pre('save', async function (next) {
-  if (this.isNew || this.isModified('password')) {
-    const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
-  }
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
 
-  next();
+    next();
 });
 
 
 userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password);
 };
 
 
 userSchema.virtual('movieCount').get(function () {
-  return this.savedMovies.length;
+    return this.savedMovies.length;
 });
 
 const User = model('User', userSchema);
