b6dc8d50551f51251b18c7ab28996b78c7def129
Dymond <dymond.hoffman03@gmail.com>
2022-12-08 17:46:55 -0800

    added pages and components

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { NextUIProvider } from '@nextui-org/react';
 import './styles/App.css';
-import Home from './pages/Home';
 
+// Import components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// Import pages
+import RentedMovies from './pages/RentedMovies';
+import SearchMovies from './pages/SearchMovies';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -14,16 +21,19 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            />
-          </Routes>
-        </div>
-      </Router>
+      <NextUIProvider>
+        <Router>
+          <>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<SearchMovies />} />
+              <Route path="/rented" element={<RentedMovies />} />
+              <Route path='*' element={<h1 className=''>Wrong page!</h1>} />
+            </Routes>
+            <Footer />
+          </>
+        </Router>
+      </NextUIProvider>
     </ApolloProvider>
   );
 }
