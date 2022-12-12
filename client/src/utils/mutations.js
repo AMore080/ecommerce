b8aa06e28765a33ab22b3b53b79a311ea9f757b7
Dymond <dymond.hoffman03@gmail.com>
2022-12-12 01:50:51 -0800

    building mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+// add user
+// login
+// rent movie
+// expire rental
+
+export const LOGIN_USER = gql`
+
+`;
+
+export const ADD_USER = gql`
+
+`;
+
+export const RENT_MOVIE = gql`
+
+`;
+
+export const EXPIRE_MOVIE = gql`
+
+`;
