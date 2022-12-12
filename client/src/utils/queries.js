98e05fa847db408929055373dc2879556cf8268a
Dymond <dymond.hoffman03@gmail.com>
2022-12-12 02:17:56 -0800

    mutations and queries updated

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,19 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            inpMovieData {
+                director
+                description
+                movieId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
