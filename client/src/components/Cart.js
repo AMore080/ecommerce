5deb4a01166eef4058966656c062c7133a7d8a66
Mona Lisa <giatran1222@gmail.com>
2022-12-14 14:48:30 -0800

    saving so far

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import Auth from '../utils/auth';
-import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js';
 require('dotenv').config();
 
 const stripePromise = loadStripe(process.env.PUBLIC_KEY)
 
 const Cart = () => {
+    
+
     return (
-        <Elements stripe={stripePromise}>
-        </Elements>
+       <div></div>
     )
 };
 
