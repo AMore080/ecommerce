6e1d462ff8079de8b16e4afd72f69ac345bfa44a
Mona Lisa <giatran1222@gmail.com>
2022-12-15 15:03:39 -0800

    actions. reducers, globalstate

---
+++ client/src/utils/actions.js
@@ -0,0 +1,5 @@
+// not sure if I need update movies but keeping in for now
+export const UPDATE_MOVIES = "UPDATE_MOVIES";
+
+export const ADD_TO_CART = "ADD_TO_CART";
+
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,23 +24,19 @@ export const ADD_USER = gql`
     }
 `;
 
-// export const RENT_MOVIE = gql`
-//      mutation rentMovie($movieData: inpMovieData!) {
-//         rentMovie(movieData: $movieData) {
-//             _id
-//             username
-//             email
-//             inpMovieData {
-//                 director
-//                 description
-//                 movieId
-//                 image
-//                 link
-//                 title
-//             }
-//         }
-//     }
-// `;
+// it might not be movies but should we use query_singlemovie or do another separate query
+export const RENT_MOVIE_ORDER = gql`
+     mutation rentMovieOrder($movies: [ID]!) {
+        rentMovieOrder(movies: $movies) {
+            purchaseDate
+            movies {
+                _id
+                original_title
+                poster_path
+            }
+        }
+    }
+`;
 
 // export const EXPIRE_MOVIE = gql`
 //      mutation expireMovie ($movieData: inpMovieData!) {
