b65b06038399c59bd560731584dc5c2a2b865a8f
Mona Lisa <giatran1222@gmail.com>
2022-12-15 18:16:30 -0800

    stash

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -2,9 +2,27 @@ import React, { useEffect} from 'react';
 import { useLazQuery } from '@apollo/client';
 import Auth from '../utils/auth';
 import { useStoreContext } from '../utils/GlobalState';
+import { ADD_TO_CART } from '../utils/actions';
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
-    const [getCheckout, {data}] = useLazyQuery(QUERY_CHECKOUT)
+    // const [getCheckout, {data}] = useLazyQuery(QUERY_CHECKOUT);
+
+    useEffect(() => {
+        async function getCart () {
+            dispatch({ type: ADD_TO_CART, movies: [...cart] });
+        }
+
+        if (state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+    return (
+        
+    )
+
 }
+
+
 
