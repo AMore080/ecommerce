2bc387c37421ecac8d1ae664c23d1f9a1a630421
Alex <alexmore080@gmail.com>
2022-12-18 11:22:45 -0600

    added loading/removed no cache

--- client/src/components/MovieCarousel.js
+++ client/src/components/MovieCarousel.js
@@ -1,19 +1,25 @@
 import React from "react";
 import Carousel from 'react-bootstrap/Carousel';
 import { useQuery } from '@apollo/client';
+import { Loading } from "@nextui-org/react";
 import { QUERY_NOWPLAYING } from "../utils/queries";
 
 const MovieCarousel = () => {
-    const { loading, data } = useQuery(QUERY_NOWPLAYING, {
-        fetchPolicy: "no-cache"
-    });
+    //Removed no-cache policy here as it makes sense to cache these as
+    // this is not constantly changing like the search results
+    // This means that the carousel will not have to fetch the data again
+    // as it will be cached instead 
+    const { loading, data } = useQuery(QUERY_NOWPLAYING);
 
     const nowPlayingList = data?.movies || [];
 
     return (
         <>
             {loading ? (
-                <div>Loading...</div>
+            <Loading
+            css={{margin: 'auto'}}
+            loadingCss={{ $$loadingSize: "100px", $$loadingBorder: "10px"}}
+          />
             ) : (
                 <Carousel fade className="mb-5">
                     {nowPlayingList.map((movie) => {
