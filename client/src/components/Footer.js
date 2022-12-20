9e6b831ab5e2c46f81f9aaa78020cc878b309b04
Dymond <dymond.hoffman03@gmail.com>
2022-12-19 16:20:30 -0800

    removed stripe button'

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,26 +1,6 @@
 import React from 'react';
 
-import axios from 'axios';
-import Stripe from "react-stripe-checkout";
-
 const Footer = () => {
-
-  const stripePublishkey = process.env.STRIPE_PUBLISH_KEY;
-
-  const handleToken = (totalAmount, token) => {
-    try {
-      axios.post("http://localhost:5000/api/stripe/pay", {
-        token: token.id,
-        amount: totalAmount
-      });
-    } catch (error) {
-      console.log(error);
-    };
-  };
-
-  const tokenHandler = (token) => {
-    handleToken(100, token);
-  }
 
   return (
     <footer>
@@ -28,14 +8,6 @@ const Footer = () => {
         © 2022
       </h3>
       <div className='text-center pb-4'>
-        < >
-          <Stripe
-            className='stripeBtn'
-            stripeKey={`${stripePublishkey}`}
-            token={tokenHandler}
-          />
-        </ >
-        <br></br>
         <button type="button" id="installBtn">
           Click to Install!
         </button>
